58e83147414d504975ced200e7561b6a3b680865
Greg <greg.knudsen09@gmail.com>
2015-07-27 13:20:14 -0400

    require http to access internet. add error message

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+
+//Problem: We need a simple way to look at a user's badge count and JS points
+//Solution: Use Node.js to connect to Treehouse api to get get profile information to print out
+
+var http = require("http");
+var username = "chalkers";
+
+function printMessage(username, badgeCount, points) {
+  var message = username + " has " + badgeCount + " total badge(s) amd " + points + " points in Javascript.";
+  console.log(message);
+}
+
+//Connect to API URL (http://teamtreehouse.com/username.json)
+var request = http.get('teamtreehouse.com/' + username + '.json', function(response){
+  console.dir(response.statusCode);
+//Read the data
+
+//Parse the data
+//Print the data
+});
+
+request.on("error", function(error){
+  console.log("Problem with request: " + error.message);
+});
