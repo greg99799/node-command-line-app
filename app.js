c3a88d2f1c18f1e72de1a06a58db8fa97451bc44
Greg <greg.knudsen09@gmail.com>
2015-07-27 14:06:33 -0400

    jumping into sandbox...trying to convert the body into an object - not successful yet

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 //Solution: Use Node.js to connect to Treehouse api to get get profile information to print out
 
 var http = require("http");
-var username = "chalkers";
+// var $ = require("jQuery");
+var username = "gregknudsen";
 
 function printMessage(username, badgeCount, points) {
   var message = username + " has " + badgeCount + " total badge(s) amd " + points + " points in Javascript.";
@@ -12,10 +13,21 @@ function printMessage(username, badgeCount, points) {
 
 //Connect to API URL (http://teamtreehouse.com/username.json)
 var request = http.get('http://teamtreehouse.com/' + username + '.json', function(response){
-  console.dir(response.statusCode);
-//Read the data
+  var body = "";
+  // console.log('HEADERS: ' + JSON.stringify(response.headers.server));
+  response.setEncoding('utf8');
+  //Read the data
+  response.on('data', function(chunk){
+    body += chunk;
+    // console.log(body);
+    // printMessage(username, chunk.badges, chunk.points.JavaScript)
+  });
+  response.on('end', function(){
+    //Parse the data
+    JSON.parse(body);
+    console.log(typeof body);
+  });
 
-//Parse the data
 //Print the data
 });
 
