4d120301b7d732ee2d080f1bc3152f59abd291b9
Greg <greg.knudsen09@gmail.com>
2015-07-27 14:44:04 -0400

    having issue with error handling - adding response.statusCode fixes it (did not have .statusCode)

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ var http = require("http");
 // var $ = require("jQuery");
 var username = "gregknudsen";
 
+//Print out message
 function printMessage(username, badgeCount, points) {
-  var message = username + " has " + badgeCount + " total badge(s) amd " + points + " points in Javascript.";
+  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in Javascript.";
   console.log(message);
+}
+
+//Print out error messages
+function printError(error){
+  console.error("We have a problem: " + error.message);
 }
 
 //Connect to API URL (http://teamtreehouse.com/username.json)
@@ -20,17 +26,25 @@ var request = http.get('http://teamtreehouse.com/' + username + '.json', functio
   response.on('data', function(chunk){
     body += chunk;
     // console.log(body);
-    // printMessage(username, chunk.badges, chunk.points.JavaScript)
   });
   response.on('end', function(){
-    //Parse the data
-    var parsedBody = JSON.parse(body); // DOES work! assign it to a variable ya jackass
-    console.log(typeof parsedBody);
+    if (response.statusCode === 200) {
+      try {
+        //Parse the data
+        var parsedBody = JSON.parse(body); // DOES work! assign it to a variable ya jackass
+        // console.log(parsedBody.profile_name);
+        //Print the data
+        printMessage(username, parsedBody.badges.length, parsedBody.points.JavaScript);
+      } catch (error) {
+          //Parse Error
+          printError({message: "parsing error"});
+        }  
+    } else {
+        //Status Code error
+        printError({message: "There was an error in getting profile " + username});
+    }
   });
-
-//Print the data
+    // console.log(response.statusCode)
+    request.on("error", printError);
 });
 
-request.on("error", function(error){
-  console.error("Problem with connection: " + error.message);
-});
