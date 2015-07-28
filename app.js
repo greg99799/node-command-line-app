f677882a898cab4de556847d0e2cef8ee9a52ef5
Greg <greg.knudsen09@gmail.com>
2015-07-28 14:35:08 -0400

    change the variable name

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var profile = require("./profile")
 // console.dir(process)
 
 // var users = ["gregknudsen123", "chalkers", "gregknudsen"];
-var myArgs = process.argv.slice(2);
-myArgs.forEach(profile.getTheThingy);
+var users = process.argv.slice(2);
+users.forEach(profile.getTheThingy);
 
